3e1d3e3e4040e50959a53131205901153abcd177
five0608 <152372534+five0608@users.noreply.github.com>
2024-01-05 13:41:39 +0900

    헤더반응

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,25 @@
 $(function(){
+    var $width;
+    var $height;
+    $width=$(".slider").width()
+    $height=$width*0.2
+    console.log($width)
+    console.log($height)
+    $(".slider").css({
+        height:$height
+    })
+
+    $(window).resize(function(){
+        $width=$(".slider").width()
+        $height=$width*0.2
+        console.log($width)
+        console.log($height)
+        $(".slider").css({
+            height:$height
+        })
+    })
+    
+
     var fadeInOutSlider=0;
     setInterval(function(){
         if(fadeInOutSlider<2){
